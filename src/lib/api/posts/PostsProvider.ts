2a65fc5b18e951456ab48b15626ee3a2f44b90e1
sugardaddy123 <werowkin@gmail.com>
2022-05-09 04:52:47 +0200

    Lib: API: Posts. Создал оболочку провайдера постов

--- src/lib/api/posts/PostsProvider.ts
+++ src/lib/api/posts/PostsProvider.ts
@@ -1,5 +1,17 @@
-export default class PostsProvider {
-    constructor() {
+import IProvider from "../interfaces/IProvider";
+import {TPost} from "./TPost";
 
+export default class PostsProvider implements IProvider {
+    getItem(id: number): TPost {
+        return {
+            id: 1,
+            userId: 12,
+            text: 'aa',
+            pictures: ['123']
+        };
+    }
+
+    getItems(): Array<TPost> {
+        return [];
     }
 }
---
+++ src/lib/api/posts/TPost.ts
@@ -0,0 +1,6 @@
+export type TPost = {
+    id: number,
+    userId: number,
+    text: string,
+    pictures: Array<string>
+}
