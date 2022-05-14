77e2963fd3573296dd0797a60b67bf2a728f27d6
sugardaddy123 <werowkin@gmail.com>
2022-05-14 02:53:45 +0200

    Header avatar load fix

--- src/components/main/menu/Header.tsx
+++ src/components/main/menu/Header.tsx
@@ -2,18 +2,20 @@ import styles from "./Header.module.scss";
 import arrow from "./svg/arrow.svg";
 import React from "react";
 import { Avatar } from "../base/picture/Avatar";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 type TProps = {
     avatarUrl: string;
 };
 
 export const Header = ({ avatarUrl }: TProps) => {
+    const { data: session } = useSession();
+
     return (
         <div className={styles.header}>
             <span className={styles.headerLogo}>TeamOne</span>
             <div className={styles.headerControl}>
-                <Avatar size={60} src={avatarUrl} />
+                <Avatar size={60} src={session.user.image} />
                 <button
                     className={styles.logoutButton}
                     onClick={() => signOut()}
